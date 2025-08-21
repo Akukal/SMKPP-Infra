3b52361d77d5175e87f3710b139854df6c9898bc
Your Name <you@example.com>
2025-08-21 13:17:55 +0700

    modified:   app/Http/Controllers/HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,21 +6,25 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    private $title = 'SMK Prestasi Prima';
+    private $desc = 'If Better is Possible, Good is Not Enough';
+    private $icon = 'images/smk.png';
+
     public function index()
     {
-        $title = 'SMK Prestasi Prima';
-        $desc = 'If Better is Possible, Good is Not Enough';
-        $icon = 'images/smk.png';
-
-        return view('pages.home', with([
-            'title' => $title,
-            'desc' => $desc,
-            'icon' => $icon,
-        ]));
+        return view('pages.home', [
+            'title' => $this->title,
+            'desc' => $this->desc,
+            'icon' => $this->icon,
+        ]);
     }
 
     public function virtualExperience()
     {
-        return view('pages.virtual-experience');
+        return view('pages.virtual-experience', [
+            'title' => $this->title,
+            'desc' => $this->desc,
+            'icon' => $this->icon,
+        ]);
     }
 }
